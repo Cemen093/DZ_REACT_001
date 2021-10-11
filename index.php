b9560b097f313a2ed37c2bbac3981e53e62bacf8
Cemen093 <CemenP93@gmail.com>
2021-10-11 23:37:41 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+echo '
+<form action="functions.php" method="post">
+    <label>
+        Login:
+        <input type="text" value="Login" name="Login">
+    </label>
+    </br>
+    <label>
+        Password:
+        <input type="text" value="Password" name="Password">
+    </label>
+    </br>
+    <label>
+        Email:
+        <input type="text" value="Email" name="Email">
+    </label>
+    </br>
+
+    <input type="reset" value="Reset ">
+    <input type="submit" value="Send">
+    </br>
+</form>  
+';
